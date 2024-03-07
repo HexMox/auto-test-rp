bf58b5f1d1c68bc7727afec7c39a6b61d5bc5479
hexuan <hexuan@idea.edu.cn>
2024-03-07 16:43:37 +0800

    feat: 支持定时自动测试任务&报告访问

--- server/internal/cmd/cmd.go
+++ server/internal/cmd/cmd.go
@@ -2,13 +2,14 @@ package cmd
 
 import (
 	"context"
+	"os"
+	"path"
 
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
 
 	"server/internal/controller/hello"
-	"server/internal/logic/testpay"
 )
 
 var (
@@ -17,6 +18,8 @@ var (
 		Usage: "main",
 		Brief: "start http server",
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
+			pwd, _ := os.Getwd()
+
 			s := g.Server()
 			s.Group("/", func(group *ghttp.RouterGroup) {
 				group.Middleware(ghttp.MiddlewareHandlerResponse)
@@ -24,9 +27,10 @@ var (
 					hello.NewV1(),
 				)
 			})
+			s.SetIndexFolder(true)
+			s.AddStaticPath("/report", path.Join(pwd, "../playwright-report"))
 			s.Run()
 
-			testpay.Run(ctx)
 			return nil
 		},
 	}
--- server/internal/logic/autotest/premium.go
+++ server/internal/logic/autotest/premium.go
@@ -1,7 +1,9 @@
 package premium
 
 import (
+	"bytes"
 	"context"
+	"os/exec"
 	"time"
 
 	"github.com/gogf/gf/v2/frame/g"
@@ -9,14 +11,30 @@ import (
 	"github.com/gogf/gf/v2/os/gctx"
 )
 
-func Init() {
+func init() {
 	var (
 		err error
 		ctx = gctx.New()
 	)
-	_, err = gcron.AddSingleton(ctx, "* * * * * *", func(ctx context.Context) {
-		g.Log().Print(ctx, "doing")
+	p, _ := g.Cfg().Get(ctx, "patterns.autotest")
+
+	g.Log().Print(ctx, "Auto testing task croning: "+p.String())
+	_, err = gcron.AddSingleton(ctx, p.String(), func(ctx context.Context) {
+		g.Log().Print(ctx, "Auto testing premium start")
 		time.Sleep(2 * time.Second)
+
+		cmd := exec.Command("npx", "playwright", "test", "--project=premium")
+		out := new(bytes.Buffer)
+
+		cmd.Stdout = out
+		cmd.Stderr = out
+		err := cmd.Start()
+
+		if err != nil {
+			g.Log().Print(ctx, out)
+		} else {
+			g.Log().Print(ctx, `Auto testing premium failed: `+err.Error())
+		}
 	})
 	if err != nil {
 		panic(err)
